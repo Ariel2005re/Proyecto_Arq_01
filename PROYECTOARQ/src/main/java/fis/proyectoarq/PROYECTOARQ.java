6503ebf0f4e0dff38884354749cd407f70fa3284
djhuevito <aitanargg@gmail.com>
2026-05-17 13:41:30 -0500

    FASE 3 Y 4

---
+++ PROYECTOARQ/src/main/java/fis/proyectoarq/PROYECTOARQ.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fis.proyectoarq;
+
+/**
+ *
+ * @author aitan
+ */
+public class PROYECTOARQ {
+
+     public static void main(String[] args) {
+
+        String resultado;
+
+        resultado = LogicaBinaria.restaBinario(5, 3, 8);
+
+        System.out.println(resultado);
+    }
+}
